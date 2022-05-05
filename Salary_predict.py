0dddfd9358360e9569478c1461c3d8d972d29279
SHIKHA K <103320160+Shikha23K@users.noreply.github.com>
2022-05-05 13:16:58 +0530

    Add files via upload

---
+++ Salary_predict.py
@@ -0,0 +1,42 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df=pd.read_csv('Salary_Data.csv')
+
+X=np.asarray(df.YearsExperience)
+y=np.asarray(df.Salary)
+
+dSet=np.random.permutation(np.arange(y.size))
+
+nTrain=int(X.size * .7)
+print(dSet.size,nTrain)
+nTest=dSet.size-nTrain
+XTrain=X[dSet[:nTrain]]
+YTrain=y[dSet[:nTrain]]
+XTest=X[dSet[nTrain:]]
+YTest=y[dSet[nTrain:]]
+
+# Training the Simple Linear Regression model on the Training set
+m= LinearRegression()
+m.fit(XTrain[:,np.newaxis],YTrain)# modeling data
+
+#predicting salary using Training set
+Y_p_train=m.predict(XTrain[:,np.newaxis])
+
+# Predicting the Test set results
+Y_p=m.predict(XTest[:,np.newaxis])#predicting salary
+
+print(m.coef_)
+
+# Visualising the Training set results
+plt.scatter(XTrain,YTrain)
+plt.plot(XTrain,Y_p_train,color='brown')
+plt.show()
+ 
+# Visualising the Test set results
+plt.scatter(XTest,YTest)
+plt.plot(XTest,Y_p,color='red')
+plt.show()
+
